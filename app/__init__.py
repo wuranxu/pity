f47db7a1190ec71a31eb2570cddaa161659dbf09
wuranxu <wuranxu@126.com>
2021-03-10 00:35:41 +0800

    支持跨域

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 from config import Config
 
 pity = Flask(__name__)
-
+CORS(pity, supports_credentials=True)
 
 pity.config.from_object(Config)
