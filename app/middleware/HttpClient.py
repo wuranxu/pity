22a40b756c074784880807a90ffb3cd907f1f867
wuranxu <wuranxu@126.com>
2021-03-14 23:38:42 +0800

    修复headers不存在时产生的bug

--- app/middleware/HttpClient.py
+++ app/middleware/HttpClient.py
@@ -37,7 +37,7 @@ class Request(object):
             if status_code != 200:
                 return Request.response(False, status_code)
             elapsed = Request.get_elapsed(response.elapsed)
-            data = response.json()
+            data = self.get_response(response)
             return Request.response(True, 200, data, response.headers, response.request.headers, elapsed=elapsed)
         except Exception as e:
             return Request.response(False, status_code, msg=str(e), elapsed=elapsed)
@@ -45,11 +45,17 @@ class Request(object):
     def post(self):
         return self.request("POST")
 
+    def get_response(self, response):
+        try:
+            return response.json()
+        except:
+            return response.text
+
     @staticmethod
     def response(status, status_code=200, response=None, response_header=None,
                  request_header=None, elapsed=None, msg="success"):
-        request_header = {k: v for k, v in request_header.items()}
-        response_header = {k: v for k, v in response_header.items()}
+        request_header = {k: v for k, v in request_header.items()} if request_header is not None else {}
+        response_header = {k: v for k, v in response_header.items()} if response_header is not None else {}
         return {
             "status": status, "response": response, "status_code": status_code,
             "response_header": response_header, "request_header": request_header,
