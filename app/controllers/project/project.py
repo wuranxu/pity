6d472a38cce14e9e6657f601edf3d63a33c61f8f
wuranxu <wuranxu@126.com>
2021-04-12 00:34:42 +0800

    新增角色增改功能

--- app/controllers/project/project.py
+++ app/controllers/project/project.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 
 from app import pity
 from app.dao.project.ProjectDao import ProjectDao
+from app.dao.project.ProjectRoleDao import ProjectRoleDao
 from app.handler.fatcory import ResponseFactory
 from app.handler.page import PageHandler
 from app.utils.decorator import permission
@@ -76,3 +77,36 @@ def query_project(user_info):
         return jsonify(dict(code=110, data=result, msg=err))
     result.update({"project": ResponseFactory.model_to_dict(data), "roles": ResponseFactory.model_to_list(roles)})
     return jsonify(dict(code=0, data=result, msg="操作成功"))
+
+
+@pr.route("/role/insert", methods=["POST"])
+@permission()
+def insert_project_role(user_info):
+    try:
+        data = request.get_json()
+        if data.get("user_id") is None or data.get("project_role") is None or data.get("project_id") is None:
+            return jsonify(dict(code=101, msg="请求参数有误"))
+        err = ProjectRoleDao.add_project_role(data.get("user_id"), data.get("project_id"), data.get("project_role"),
+                                              user_info["id"])
+        if err is not None:
+            return jsonify(dict(code=110, msg=err))
+    except Exception as e:
+        return jsonify(dict(code=110, msg=str(e)))
+    return jsonify(dict(code=0, msg="操作成功"))
+
+
+@pr.route("/role/update", methods=["POST"])
+@permission()
+def update_project_role(user_info):
+    try:
+        data = request.get_json()
+        if data.get("user_id") is None or data.get("project_role") is None or data.get("project_id") is None \
+                or data.get("id") is None:
+            return jsonify(dict(code=101, msg="请求参数有误"))
+        err = ProjectRoleDao.update_project_role(data.get("id"), data.get("project_role"),
+                                                 user_info["id"], user_info["role"])
+        if err is not None:
+            return jsonify(dict(code=110, msg=err))
+    except Exception as e:
+        return jsonify(dict(code=110, msg=str(e)))
+    return jsonify(dict(code=0, msg="操作成功"))
--- app/dao/project/ProjectRoleDao.py
+++ app/dao/project/ProjectRoleDao.py
@@ -1,6 +1,9 @@
+from datetime import datetime
 from typing import List
 
+from app import pity
 from app.models import db
+from app.models.project import Project
 from app.models.project_role import ProjectRole
 from app.utils.logger import Log
 
@@ -42,8 +45,7 @@ class ProjectRoleDao(object):
         :return:
         """
         try:
-            role = ProjectRole.query.filter_by(user_id=user_id, project_id=project_id, project_role=project_role,
-                                               deleted_at=None).first()
+            role = ProjectRole.query.filter_by(user_id=user_id, project_id=project_id, deleted_at=None).first()
             if role is not None:
                 # 说明角色已经存在了
                 return "该用户已存在"
@@ -53,4 +55,29 @@ class ProjectRoleDao(object):
         except Exception as e:
             ProjectRoleDao.log.error(f"添加项目用户失败, {e}")
             return f"添加项目用户失败, {e}"
+        return None
+
+    @staticmethod
+    def update_project_role(role_id, project_role, user, user_role):
+        try:
+            role = ProjectRole.query.filter_by(id=role_id, deleted_at=None).first()
+            if role is None:
+                return "该用户角色不存在"
+            if user_role != pity.config.get("ADMIN"):
+                project = Project.query.filter_by(id=role.project_id).first()
+                if project is None:
+                    return "该项目不存在"
+                if project.owner != user and role.project_role == 1:
+                    # 说明既不是owner也不是超管，无法修改阻止的权限
+                    return "不能修改其他组长的权限"
+            updater_role = ProjectRole.query.filter_by(user_id=user, project_id=role.project_id, deleted_at=None).first()
+            if updater_role is None or updater_role.project_role == 1:
+                return "对不起，你没有权限"
+            role.project_role = project_role
+            role.updated_at = datetime.now()
+            role.update_user = user
+            db.session.commit()
+        except Exception as e:
+            ProjectRoleDao.log.error(f"修改项目用户失败, {e}")
+            return f"修改项目用户失败, {e}"
         return None
