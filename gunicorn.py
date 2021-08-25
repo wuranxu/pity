af938b087c155a3dbe8ba3f3235b032b2c2986ce
woody.wu <619434176@qq.com>
2021-08-25 23:07:54 +0800

    gunicorn

---
+++ gunicorn.py
@@ -0,0 +1,22 @@
+# gunicorn的配置
+import os
+import gevent.monkey
+
+gevent.monkey.patch_all()
+
+import multiprocessing
+
+# debug = True
+loglevel = 'debug'
+bind = "0.0.0.0:4000"
+pidfile = "logs/gunicorn.pid"
+accesslog = "logs/access.log"
+errorlog = "logs/debug.log"
+daemon = True
+timeout = 60
+
+# 启动的进程数
+# workers = multiprocessing.cpu_count()
+workers = 8
+worker_class = 'gevent'
+x_forwarded_for_header = 'X-FORWARDED-FOR'
