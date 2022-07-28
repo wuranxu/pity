9d2b5c55d6f23920aa506c3cc37f947d8c9884d2
rx.wu <rx.wu@trip.com>
2022-07-28 20:27:28 +0800

    修复项目联合索引问题

--- app/models/project.py
+++ app/models/project.py
@@ -1,17 +1,20 @@
-from sqlalchemy import INT, Column, String, BOOLEAN
+from sqlalchemy import INT, Column, String, BOOLEAN, UniqueConstraint
 
 from app.models.basic import PityBase
 
 
 class Project(PityBase):
     __tablename__ = 'pity_project'
-    name = Column(String(16), unique=True, index=True)
+    name = Column(String(16))
     owner = Column(INT)
-    app = Column(String(32), index=True)
+    app = Column(String(32))
     private = Column(BOOLEAN, default=False)
     description = Column(String(200))
     avatar = Column(String(128), nullable=True)
     dingtalk_url = Column(String(128), nullable=True)
+    __table_args__ = (
+        UniqueConstraint('name', 'deleted_at'),
+    )
     __tag__ = "项目"
     __fields__ = (name, owner, app, private, description, avatar, dingtalk_url)
     __alias__ = dict(name="项目名称", owner="项目所有者", app="项目所属应用", private="是否私有", description="项目描述", avatar="项目头像",
